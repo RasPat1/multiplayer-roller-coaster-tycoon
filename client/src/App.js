ba71043ca8a4dc17d56249ab659b3ef3481978dd
cquinones100 <cquinones100@gmail.com>
2018-05-05 23:57:02 -0400

    adds logic for placing items on the grid

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,59 @@ import WithMouseCoordinates from './components/WithMouseCoordinates'
 
 export const MousePositionContext = React.createContext({})
 
+export const SelectedItemContext = React.createContext({})
+
+class MouseTrackerComponent extends Component {
+
+  render() {
+    const { x:left, y:top } = this.props
+    return (
+      <div style={{left: left + 10, top: top + 10, position: 'absolute'}}>
+        {this.props.render()}
+      </div>
+    )
+  }
+}
+
 class App extends Component {
+  state = { mouseTracker: null, selectedHudItem: null }
+
+  setMouseTrailerComponent(component) {
+    this.setState({
+      mouseTracker: component,
+      selectedHudItem: 'kiosk'
+    })
+  }
+
   render() {
     return (
       <WithMouseCoordinates
         render={(x, y) =>
-          <MousePositionContext.Provider coordinates={{x, y}}>
-            <div className="App">
-              <header className="App-header">
-                <h1 className="App-title">Roller Coaster Tycoon</h1>
-              </header>
-              <p className="App-intro">
-                Hey its a game.
-              </p>
-              <Board />
-              <Hud />
-            </div>
+          <MousePositionContext.Provider
+            value={{
+              coordinates: { x, y },
+              setMouseTrailerComponent: component =>
+                this.setMouseTrailerComponent(component)
+            }}
+          >
+            <SelectedItemContext.Provider value={this.state.selectedHudItem}>
+              <div className="App">
+                <header className="App-header">
+                  <h1 className="App-title">Roller Coaster Tycoon</h1>
+                </header>
+                <p className="App-intro">
+                  Hey its a game.
+                </p>
+                {this.state.mouseTracker &&
+                  <MouseTrackerComponent
+                    x={x}
+                    y={y}
+                    render={() => this.state.mouseTracker} />
+                }
+                <Board />
+                <Hud />
+              </div>
+            </SelectedItemContext.Provider>
           </MousePositionContext.Provider>
         }
       />
