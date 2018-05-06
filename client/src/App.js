bed02d79983c2d13552fdaa4b4693e188526d48d
cquinones100 <cquinones100@gmail.com>
2018-05-05 23:04:42 -0400

    adds withmousecoordinates to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import Board from './components/Board/Board'
+import Hud from './components/Hud/Hud'
+import WithMouseCoordinates from './components/WithMouseCoordinates'
+
+export const MousePositionContext = React.createContext({})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Roller Coaster Tycoon</h1>
-        </header>
-        <p className="App-intro">
-          Hey its a game.
-        </p>
-        <Board />
-      </div>
+      <WithMouseCoordinates
+        render={(x, y) =>
+          <MousePositionContext.Provider coordinates={{x, y}}>
+            <div className="App">
+              <header className="App-header">
+                <h1 className="App-title">Roller Coaster Tycoon</h1>
+              </header>
+              <p className="App-intro">
+                Hey its a game.
+              </p>
+              <Board />
+              <Hud />
+            </div>
+          </MousePositionContext.Provider>
+        }
+      />
     );
   }
 }
