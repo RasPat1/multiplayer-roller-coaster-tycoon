7aec2b6e0012a73dc1c127a86580796243e84329
Rasesh Patel <raspat1@gmail.com>
2018-05-06 02:05:36 -0400

    Add state shape to docs

---
+++ docs/state_shape.js
@@ -0,0 +1,15 @@
+{
+    money: 10000,
+    item: [
+        {
+            name: 'kiosk'
+            id: 1,
+            location: [1,1]
+        },
+        {
+            name: 'kiosk'
+            id: 123,
+            location: [6,7]
+        }
+    ]
+}
