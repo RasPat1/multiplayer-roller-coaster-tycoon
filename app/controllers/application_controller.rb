8cfeeb9e01fcb5a331cdf8fd60b1cdfef20a4907
cquinones100 <cquinones100@gmail.com>
2018-05-05 23:08:24 -0400

    sets up fallback route for rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ class ApplicationController < ActionController::API
     heartbeat = { alive: true }
     json_response(heartbeat)
   end
+
+  def fallback_index_html
+  respond_to do |format|
+    format.html { render body: Rails.root.join('public/index.html').read }
+  end
+end
 end
